67cc16151765fcbc930c553398ba0efac0fee7b3
wayne <waynehui77@hotmail.com>
2015-11-05 00:54:23 +1100

    update retailer

--- beaconscan/Utils/Retailer.swift
+++ beaconscan/Utils/Retailer.swift
@@ -15,17 +15,19 @@ class Retailer {
     var location : String!
     var telephone : String!
     var cover_photo : String!
+    var thumbnail : String!
     var short_description :  String!
     var long_description : String!
     var status : String!
     
     init(data : NSDictionary){
         
-        self.retail_id = data["retail_id"] as! Int
+        self.retail_id = data["retailer_id"] as! Int
         self.name = Utils.getStringFromJSON(data, key: "name")
         self.location = Utils.getStringFromJSON(data, key: "location")
         self.telephone = Utils.getStringFromJSON(data, key: "telephone")
         self.cover_photo = Utils.getStringFromJSON(data, key: "cover_photo")
+        self.thumbnail = Utils.getStringFromJSON(data, key: "thumbnail")
         self.short_description = Utils.getStringFromJSON(data, key: "short_description")
         self.long_description = Utils.getStringFromJSON(data, key: "long_description")
         self.status = Utils.getStringFromJSON(data, key: "status")
