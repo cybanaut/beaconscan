0a84305b2c35423eecc6ea67287a2d104a77daf5
wayne <waynehui77@hotmail.com>
2015-11-06 02:43:52 +1100

    Setup setting view controller

--- beaconscan/view/SettingViewController.swift
+++ beaconscan/view/SettingViewController.swift
@@ -12,6 +12,9 @@ class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerV
 
     @IBOutlet weak var menuButton: UIBarButtonItem!
     @IBOutlet weak var ageGroupPicker: UIPickerView!
+    @IBOutlet weak var demoModeLabel: UILabel!
+    @IBOutlet weak var selectModeSwitch: UISwitch!
+    @IBOutlet weak var genderSegment: UISegmentedControl!
     
     let ageGroup = ["","Under 20","20-29","30-39","40-49","50-59","60+"]
     
@@ -20,6 +23,7 @@ class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerV
         ageGroupPicker.dataSource = self
         ageGroupPicker.delegate = self
         
+        selectModeSwitch.addTarget(self, action: Selector("stateChanged:"), forControlEvents: UIControlEvents.ValueChanged)
         
         if self.revealViewController() != nil {
             menuButton.target = self.revealViewController()
@@ -33,6 +37,29 @@ class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerV
         // Dispose of any resources that can be recreated.
     }
     
+    func stateChanged(switchState: UISwitch) {
+        if switchState.on {
+            print("Special Offer Mode")
+            demoModeLabel.text = "Demo Mode: Special Offer"
+        } else {
+            print("Lai See Mode")
+            demoModeLabel.text = "Demo Mode: Lai See"
+        }
+    }
+    
+    @IBAction func selectGender(sender: UISegmentedControl) {
+        switch genderSegment.selectedSegmentIndex
+        {
+        case 0:
+            print("Male")
+        case 1:
+            print("Female")
+        case 2:
+            print("Not Disclosed")
+        default:
+            break; 
+        }
+    }
 
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 1
