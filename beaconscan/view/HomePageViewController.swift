0f27a084b161fda6e26746ce9ac98010e3a87953
wayne <waynehui77@hotmail.com>
2015-11-08 03:41:11 +1100

    homepage all working

--- beaconscan/view/HomePageViewController.swift
+++ beaconscan/view/HomePageViewController.swift
@@ -7,25 +7,64 @@
 //
 
 import UIKit
+import CoreLocation
 
-class HomePageViewController: UIViewController {
+class HomePageViewController: UIViewController, CLLocationManagerDelegate  {
 
     @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    let locationManager = CLLocationManager()
+    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "F94DBB23-2266-7822-3782-57BEAC0952AC")!, identifier: "Beaconstac")
+    let spinner = UIActivityIndicatorView( activityIndicatorStyle: UIActivityIndicatorViewStyle.White)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.view.addBackground("HomePage.png")
+
+        let switchButton = UIBarButtonItem(title: "Start", style: .Plain, target: self, action: "switchTapped")
+        
+        navigationItem.rightBarButtonItems=[switchButton, UIBarButtonItem(customView: spinner)]
+        navigationController?.navigationBar.barTintColor = UIColor.redColor()
         // to make side menu work
         if self.revealViewController() != nil {
             menuButton.target = self.revealViewController()
             menuButton.action = "revealToggle:"
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        locationManager.delegate=self;
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse) {
+            locationManager.requestWhenInUseAuthorization()
+            
+        }
+        locationManager.startRangingBeaconsInRegion(region)
 
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func switchTapped(){
+        spinner.startAnimating()
+    }
+    
+    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
+        if (knownBeacons.count > 0) {
+            let closestBeacon = knownBeacons[0] as CLBeacon
+            //            self.view.backgroundColor = self.colours[closestBeacon.minor.integerValue]
+            //            print(closestBeacon);
+            print(closestBeacon.proximityUUID.UUIDString);
+            print(closestBeacon.major);
+            print(closestBeacon.minor);
+            print(closestBeacon.rssi);
+            //           print(region);
+            //           print(region.proximityUUID);
+            //           print(region.identifier);
+        }
     }
     
 
